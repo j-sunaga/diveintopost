8b900697e3d96637bd31f32b2282988770ad66ff
j-sunaga <124577blue@gmail.com>
2020-06-21 14:40:16 +0900

    プロフィール欄追加のマイグレーションの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_035232) do
+ActiveRecord::Schema.define(version: 2020_06_21_053733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2018_10_05_035232) do
     t.datetime "updated_at", null: false
     t.string "icon"
     t.integer "keep_team_id"
+    t.text "content"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
